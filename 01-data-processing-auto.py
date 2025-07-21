f29ceae0751cfc65ab0dc7d3b3c7debed2924aa3
Dan Dan <ddanzk28@gmail.com>
2025-07-20 23:31:10 -0700

    Add KNN imputer for pol_edeliv_ind and Separate auto and nonauto into two different files

--- 01-data-processing-auto.py
+++ 01-data-processing-auto.py
@@ -27,21 +27,6 @@ test = pd.read_csv("data/test_data.csv")
 random_state = 42
 
 # Step 1: Split data into NonAuto and Auto (NonAuto doesn't need bi_limit_group, newest_veh_age, telematics_ind)
-def non_auto(data):
-    conn = duckdb.connect()
-    conn.register("data", data)
-    nonauto_query = """
-        WITH cte AS (
-            SELECT * FROM data 
-            WHERE bi_limit_group = 'NonAuto' AND newest_veh_age = -20 AND telematics_ind = -2
-        )
-        SELECT * EXCLUDE (bi_limit_group, newest_veh_age, telematics_ind)
-        FROM cte;
-    """
-    df = conn.execute(nonauto_query).fetch_df()
-    conn.close()
-    return df 
-
 def auto(data):
     conn = duckdb.connect()
     conn.register("data", data)
@@ -53,12 +38,12 @@ def auto(data):
     conn.close()
     return df
 
-auto_train = auto(train)
-non_auto_train = non_auto(train)
+auto_train_df = auto(train)
+auto_test_df = auto(test)
 
 # Step 2: Split train into train and validation (Auto)
-X = auto_train.drop(["call_counts" , "id"], axis=1)
-y = auto_train["call_counts"]
+X = auto_train_df.drop(["call_counts" , "id"], axis=1)
+y = auto_train_df["call_counts"]
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=random_state)
 
@@ -119,11 +104,11 @@ def knn_prep(X):
 
 
 def impute_df(X, acq_method_imputed):
-    X = X.copy()
-    X["index"] = X.index
+    data = X.copy()
+    data["index"] = data.index
 
     conn = duckdb.connect()
-    conn.register("X", X)
+    conn.register("data", data)
     conn.register("acq_method_imputed", acq_method_imputed)
 
     query = """
@@ -140,12 +125,12 @@ def impute_df(X, acq_method_imputed):
                 END
             ELSE a.acq_method
         END AS acq_method_filled
-    FROM X AS a
+    FROM data AS a
     LEFT JOIN acq_method_imputed AS b
     ON a.index = b.index)
-    SELECT * FROM cte;
-    --SELECT * EXCLUDE (acq_method)
-    --FROM cte;
+
+    SELECT * EXCLUDE (acq_method)
+    FROM cte;
     """
     X_final = conn.execute(query).fetch_df()
     X_final.set_index("index", inplace=True)
@@ -165,7 +150,7 @@ y_pred = knn_imputer.predict(X_test_knn)
 y_test_knn["acq_method_encoded"] = y_pred
 acq_method_imputed = pd.concat([y_train_knn, y_test_knn], axis=0)
 
-X_train_final = impute_df(X_train, acq_method_imputed)
+X_train = impute_df(X_train, acq_method_imputed)
 
 
 
@@ -175,17 +160,129 @@ y_pred = knn_imputer.predict(X_test_knn)
 y_test_knn["acq_method_encoded"] = y_pred
 acq_method_imputed = pd.concat([y_train_knn, y_test_knn], axis=0)
 
-X_val_final = impute_df(X_val, acq_method_imputed)
+X_val = impute_df(X_val, acq_method_imputed)
 
 # Impute test acq_method
-_, y_train_knn, X_test_knn, y_test_knn = knn_prep(test)
+_, y_train_knn, X_test_knn, y_test_knn = knn_prep(auto_test_df)
 y_pred = knn_imputer.predict(X_test_knn)
 y_test_knn["acq_method_encoded"] = y_pred
 acq_method_imputed = pd.concat([y_train_knn, y_test_knn], axis=0)
 
-test_final = impute_df(test, acq_method_imputed)
+auto_test_df = impute_df(auto_test_df, acq_method_imputed)
 
 
-X_train_final.to_csv("train_checking.csv")
-X_val_final.to_csv("val_checking.csv")
-test_final.to_csv("testing.csv")
+
+# Prepare data for imputing pol_edeliv_ind (-2 and -1 as missing) using KNN
+def knn_prep(X):
+    data = X.copy()
+    data["index"] = X.index
+
+    conn = duckdb.connect()
+    conn.register("data", data)
+    
+    query = """
+    CREATE OR REPLACE TABLE knn_prep AS
+        SELECT 
+            index, -- keeping track of index
+            "12m_call_history",
+            ann_prm_amt,
+            home_lot_sq_footage,
+            household_policy_counts,
+            newest_veh_age,
+            tenure_at_snapshot,
+            trm_len_mo,
+            CAST(CASE 
+                WHEN pol_edeliv_ind = 0 THEN 0
+                WHEN pol_edeliv_ind = 1 THEN 1
+                ELSE NULL 
+            END AS INTEGER) AS pol_edeliv_ind_encoded
+        FROM data;
+    """
+    conn.execute(query)
+    train = """
+        SELECT * FROM knn_prep
+        WHERE pol_edeliv_ind_encoded IS NOT NULL;
+    """
+    test = """
+        SELECT * FROM knn_prep
+        WHERE pol_edeliv_ind_encoded IS NULL;
+    """
+
+    train = conn.execute(train).fetch_df()
+    test = conn.execute(test).fetch_df()
+
+    X_train = train.drop(["pol_edeliv_ind_encoded", "index"], axis=1)
+    y_train = train[["index","pol_edeliv_ind_encoded"]] # has values for the target
+
+    X_test = test.drop(["pol_edeliv_ind_encoded", "index"], axis=1)
+    y_test = test[["index","pol_edeliv_ind_encoded"]] # need to be imputed
+
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, y_train, X_test, y_test
+
+
+def impute_df(X, pol_edeliv_ind_imputed):
+    data = X.copy()
+    data["index"] = data.index
+
+    conn = duckdb.connect()
+    conn.register("data", data)
+    conn.register("pol_edeliv_ind_imputed", pol_edeliv_ind_imputed)
+
+    query = """
+    WITH cte AS (SELECT 
+        a.*,
+        CASE 
+            WHEN a.pol_edeliv_ind IN (-1, -2) THEN b.pol_edeliv_ind_encoded
+            ELSE a.pol_edeliv_ind
+        END AS pol_edeliv_ind_filled
+    FROM data AS a
+    LEFT JOIN pol_edeliv_ind_imputed AS b
+    ON a.index = b.index)
+
+    SELECT * EXCLUDE (pol_edeliv_ind)
+    FROM cte;
+    """
+    X_final = conn.execute(query).fetch_df()
+    X_final.set_index("index", inplace=True)
+    conn.close()
+    return X_final
+
+
+
+# Impute X_train acq_method
+X_train_knn, y_train_knn, X_test_knn, y_test_knn = knn_prep(X_train)
+
+# Train KNNClassifier imputer
+knn_imputer = KNeighborsClassifier(n_neighbors=5)  # you can tune this
+knn_imputer.fit(X_train_knn, y_train_knn["pol_edeliv_ind_encoded"].values)
+
+y_pred = knn_imputer.predict(X_test_knn)
+y_test_knn["pol_edeliv_ind_encoded"] = y_pred
+pol_edeliv_ind_imputed = pd.concat([y_train_knn, y_test_knn], axis=0)
+
+X_train = impute_df(X_train, pol_edeliv_ind_imputed)
+
+
+
+# Impute X_val acq_method
+_, y_train_knn, X_test_knn, y_test_knn = knn_prep(X_val)
+y_pred = knn_imputer.predict(X_test_knn)
+y_test_knn["pol_edeliv_ind_encoded"] = y_pred
+pol_edeliv_ind_imputed = pd.concat([y_train_knn, y_test_knn], axis=0)
+
+X_val = impute_df(X_val, pol_edeliv_ind_imputed)
+
+# Impute test acq_method
+_, y_train_knn, X_test_knn, y_test_knn = knn_prep(auto_test_df)
+y_pred = knn_imputer.predict(X_test_knn)
+y_test_knn["pol_edeliv_ind_encoded"] = y_pred
+pol_edeliv_ind_imputed = pd.concat([y_train_knn, y_test_knn], axis=0)
+
+auto_test_df = impute_df(auto_test_df, pol_edeliv_ind_imputed)
+
+
+
