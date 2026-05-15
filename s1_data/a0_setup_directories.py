db5c3de92e15e0c8ade5b7aae82771db9beb14f9
Dan Dan <ddan@VDD-DDAN-LX.pnmac.com>
2026-05-15 11:04:33 -0700

    s1_data/a0_setup_directories.py: add set up dir script

---
+++ s1_data/a0_setup_directories.py
@@ -0,0 +1,10 @@
+import os
+
+plot_dir = "plots"
+os.makedirs(plot_dir, exist_ok=True)
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+model_dir = "models"
+os.makedirs(model_dir, exist_ok=True)
